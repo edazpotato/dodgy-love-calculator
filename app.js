d7a1a778fd63f4f79f74a6e14bd3968e3d1ec636
Edaz <edazpotato@gmail.com>
2021-04-15 15:16:40 +1200

    Use new

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function setup() {
 		var name1 = input1.value;
 		var name2 = input2.value;
 		//var percent = Math.floor(Math.random() * 100) + 1;
-		var rng = Math.seedrandom(name1+name2);
+		var rng = new Math.seedrandom(name1+name2);
 		var percent = (rng()*100)+1;
 		var html = '<img class="output--image" src="heart.png" /><h3 class="output--names">'+name1+' & '+name2+'</h3><p class="output--text">are</p><h2 class="output--percent">'+percent+'%</h2><p class="output--text">compatible!</p>'
 		output.innerHTML = html;
