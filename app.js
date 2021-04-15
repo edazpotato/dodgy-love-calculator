a21263c1fed58df2ef949c4127c19b453fc65b78
Edaz <edazpotato@gmail.com>
2021-04-15 15:20:02 +1200

    Whoops

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ function setup() {
 		var name1 = input1.value;
 		var name2 = input2.value;
 		//var percent = Math.floor(Math.random() * 100) + 1;
-		var rng = Math.round(new Math.seedrandom(name1+name2));
-		var percent = (rng()*100)+1;
+		var rng = new Math.seedrandom(name1+name2);
+		var percent = Math.round((rng()*100)+1);
 		var html = '<img class="output--image" src="heart.png" /><h3 class="output--names">'+name1+' & '+name2+'</h3><p class="output--text">are</p><h2 class="output--percent">'+percent+'%</h2><p class="output--text">compatible!</p>'
 		output.innerHTML = html;
 	});
